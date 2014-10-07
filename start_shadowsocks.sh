3088b543673a43e0355c9ade09c206e42fb545e3
WinkChow <yestab123@gmail.com>
2014-10-07 17:14:04 +0800

    Update start_shadowsocks.sh

--- start_shadowsocks.sh
+++ start_shadowsocks.sh
@@ -1,3 +1,3 @@
 #!/bin/bash                                                                                                                          
-2
-3ssserver -c /etc/shadowsocks.json
+
+ssserver -c /etc/shadowsocks.json

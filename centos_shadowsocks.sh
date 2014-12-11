f85138333e1b965daaa4e6122031aea08955b046
WinkChow <yestab123@gmail.com>
2014-12-11 16:50:29 +0800

    Update centos_shadowsocks.sh

--- centos_shadowsocks.sh
+++ centos_shadowsocks.sh
@@ -13,7 +13,7 @@ pip install shadowsocks
 
 file="{
     \"server\":\"0.0.0.0\",
-    \"server_port\":12888,
+    \"server_port\":80,
     \"local_address\": \"127.0.0.1\",
     \"local_port\":1080,
     \"password\":\"3810171\",

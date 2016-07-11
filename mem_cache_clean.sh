05fbd8a24f1fac901dbd8bd6e486a6aa379920b7
WinkChow(Ziv Chow) <yestab123@gmail.com>
2016-07-11 11:01:40 +0800

    Optimize script

--- mem_cache_clean.sh
+++ mem_cache_clean.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-sync
+
 free -m
-echo 1 > /proc/sys/vm/drop_caches
+sync
+sleep 1
 echo 3 > /proc/sys/vm/drop_caches
 free -m
+
+#To free pagecache:
+#* echo 1 > /proc/sys/vm/drop_caches
+#To free dentries and inodes:
+#* echo 2 > /proc/sys/vm/drop_caches
+#To free pagecache, dentries and inodes:
+#* echo 3 > /proc/sys/vm/drop_caches
